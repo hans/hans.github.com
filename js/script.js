5a2d1d1d7632b68253416a43ed1ff47e57fa5e23
Hans Engel <engel@engel.uk.to>
2013-12-19 15:53:19 -0700

    Add footnote tooltips with qtip

--- js/script.js
+++ js/script.js
@@ -1,26 +1,15 @@
-/* Author: 
+var container = $('#container');
+var footnotes = $('div.footnotes');
 
-*/
+$('a.footnote').each(function() {
+  var el = $(this);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  el.qtip({
+    content: {
+      text: footnotes.find('li' + el.attr('href').replace(':', '\\:') + ' p').clone()
+    },
+    position: {
+      viewport: container
+    }
+  });
+});
